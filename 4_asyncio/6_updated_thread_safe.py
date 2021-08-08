874ea72db3f83f9e05dbd52a972a8ddddd1d8077
ja-odur <odurjoseph8@gmail.com>
2021-08-08 14:25:37 +0300

    asyncio updated threadsafe example

---
+++ 4_asyncio/6_updated_thread_safe.py
@@ -0,0 +1,36 @@
+import asyncio
+from concurrent.futures import ThreadPoolExecutor
+
+
+class Message(dict):
+    def __init__(self, **content):
+        super().__init__()
+        self.content = content
+        for k, v in content.items():
+            self[k] = v
+
+    def __repr__(self):
+        return f'Message(**{self.content})'
+
+
+async def handle_message(msg):
+    print(msg)
+
+
+def handle_message_sync(loop, **content):
+    msg = Message(**content)
+    print(f'Pulled {msg}')
+    loop.create_task(handle_message(msg))
+
+def threaded_consumer(loop):
+    print('running threaded consumer')
+    handle_message_sync(loop, **{'to': 'test', 'from': 'from test', 'message': 'test msg'})
+
+
+async def main():
+    loop = asyncio.get_running_loop()
+    executor = ThreadPoolExecutor()
+    await loop.run_in_executor(executor, threaded_consumer, loop)
+
+
+asyncio.run(main(), debug=True)
